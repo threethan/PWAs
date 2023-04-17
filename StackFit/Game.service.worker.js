fd7c1dac7ffeb3e7007b9bfe99b35bfd31d292ac
threethan <ethanjmedei@gmail.com>
2023-04-17 03:41:31 -0400

    Update StackFit

--- StackFit/Game.service.worker.js
+++ StackFit/Game.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1681708735|647547803";
+const CACHE_VERSION = "1681717030|216582696";
 const CACHE_PREFIX = "Stack Fit-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Game.offline.html";
